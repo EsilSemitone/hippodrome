c399e08253977ac38b4dcf48bc71dea4a9016422
Semitone <semi_tone@mail.ru>
2022-12-22 20:27:02 +0500

    New version update Theme.py v 1.16

--- Theme.py
+++ Theme.py
@@ -4,10 +4,10 @@ class Theme():
 
     _themes = ('Green', 'Dark', 'White')
 
-    def __init__(self, Theme = _themes[0]):
+    #def __init__(self, Theme = _themes[0]):
 
-        try:
-            with open("saves\\config.json", 'r') as f:
-                json_file = json.load(f)
-                self.Theme = json_file['Theme']
+    #    try:
+    #        with open("saves\\config.json", 'r') as f:
+    #            json_file = json.load(f)
+    #            self.Theme = json_file['Theme']
                 
